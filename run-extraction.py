f7f341fe21d89ec5e43c06a3c537505b1010ff25
Robert <robikoser@gmail.com>
2020-04-17 12:37:35 +0200

    Add RTV regex

--- run-extraction.py
+++ run-extraction.py
@@ -6,22 +6,32 @@ import requests
 
 def regular_expression_rtv(pages):
     author_regex = r"<div class=\"author-name\">(.*)<\/div>"
+    published_time_regex = r"<div class=\"publish-meta\">[\n\s]*(.*)<br>"
+    title_regex = r"<header class=\"article-header\">(.|\n)*<h1>(.*)<\/h1>"
+    sub_title_regex = r"<header class=\"article-header\">(.|\n)*<div class=\"subtitle\">(.*)<\/div>"
+    lead_regex = r"<header class=\"article-header\">(.|\n)*<p class=\"lead\">[\n\s]*(.*)<\/p>"
+    #a more bit brez html tagov?
+    content_regex = r"<div class=\"article-body\">(\s|\n|.)*?<div class=\"article-column\""
 
     for page in pages:
-        match = re.compile(author_regex).search(page)
-        author = match.group(1)
-        print(author)
-        """
+        author = re.compile(author_regex).search(page).group(1)
+        published_time = re.compile(published_time_regex).search(page).group(1)
+        title = re.compile(title_regex).search(page).group(2)
+        subtitle = re.compile(sub_title_regex).search(page).group(2)
+        lead = re.compile(lead_regex).search(page).group(2)
+        content = re.compile(content_regex).search(page).group(0)
+        print(content)
         dataItem = {
-            "Author": title,
-            "PublishedTime": date,
-            "Title": imageUrl,
-            "SubTitle": subTitle,
+            "Author": author,
+            "PublishedTime": published_time,
+            "Title": title,
+            "SubTitle": subtitle,
             "Lead": lead,
             "Content": content
-        }"""
+        }
+        #print(dataItem)
 
-pageContent1 = open('rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html', 'r').read()
-pageContent2 = open('rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html', 'r').read()
+pageContent1 = open('rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html', 'r', encoding='utf8').read()
+pageContent2 = open('rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljše v razredu - RTVSLO.si.html', 'r', encoding='utf8').read()
 
 regular_expression_rtv([pageContent1, pageContent2])
